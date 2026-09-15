f561f47d584657bfbd089a7d0dd3c39e61dc1193
k-awaki <103158503+k-awaki@users.noreply.github.com>
2026-09-15 17:17:34 +0900

    Update tempus_config.tcl

--- script/tempus_config.tcl
+++ script/tempus_config.tcl
@@ -1,6 +1,6 @@
-set _MODEL      ZeroSkipOPU
+set _MODEL      counter
 set FREQ_MHz    100
-set DESIGN      top${_MODEL}
-set MMMC        mmmc/${_MODEL}/enc_default_freq${FREQ_MHz}.view
-set INNOVUS_OUTPUT innovus/${_MODEL}/freq${FREQ_MHz}/outputs
-set TEMPUS_REPORT tempus/${_MODEL}/freq${FREQ_MHz}/reports
+set DESIGN      ${_MODEL}
+set MMMC        mmmc/enc_default.view
+set INNOVUS_OUTPUT innovus/outputs
+set TEMPUS_REPORT tempus/reports
